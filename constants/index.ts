7cd4609efc3d4603be7bd5ec2b69aec12032e3e9
Nikhil Kumar <nikhil89.2005@gmail.com>
2025-09-01 10:27:05 +0530

    cloud storage, pdf to image conversion, key value Storage and AI feedback

--- constants/index.ts
+++ constants/index.ts
@@ -227,12 +227,9 @@ export const AIResponseFormat = `
 
 export const prepareInstructions = ({
   jobTitle,
-  jobDescription,
-  AIResponseFormat,
+  jobDescription
 }: {
-  jobTitle: string;
-  jobDescription: string;
-  AIResponseFormat: string;
+  jobTitle: string; jobDescription: string
 }) =>
   `You are an expert in ATS (Applicant Tracking System) and resume analysis.
   Please analyze and rate this resume and suggest how to improve it.
